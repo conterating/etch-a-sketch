4cf84661a406582e5edecef533acfc8727d79d58
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-06-28 08:54:17 -0400

    Added change grid size feature and random color

--- main.js
+++ main.js
@@ -17,3 +17,61 @@ function createGrid(gridDimensions) {
 }
 
 createGrid(16);
+
+function identifyWhenHovered() {
+  let container = document.querySelector(".container");
+  container.addEventListener("mouseover", function (e) {
+    let divBox = e.target;
+    console.log(divBox.getAttribute("class"));
+    if (divBox.getAttribute("class") == "container") {
+      console.log("container");
+    } else {
+      divBox.style.backgroundColor = getRandomColor();
+    }
+  });
+}
+
+function getRandomColor() {
+  let hexCodes = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function requestSizeChange() {
+  let header = document.createElement("h1");
+  header.textContent = "Etch A Sketch";
+  let button = document.createElement("button");
+  const buttonContainer = document.querySelector(".button-container");
+  const gridContainer = document.querySelector(".grid-container");
+  button.textContent = "Change Grid Shape";
+  button.classList.add("grid-button");
+  buttonContainer.appendChild(header);
+  buttonContainer.appendChild(button);
+  gridContainer.appendChild(buttonContainer);
+}
+
+function gridButtonClicked() {
+  let button = document.querySelector(".grid-button");
+  let container = document.querySelector(".container");
+  button.addEventListener("click", (e) => {
+    let userGridSize = prompt("Enter a grid size: (Max Size = 100)");
+    if (userGridSize > 100) {
+      alert("Grid Size is too big, please choose a number under 100");
+    } else {
+      container.innerHTML = "";
+      createGrid(userGridSize);
+    }
+  });
+}
+
+function playGame() {
+  identifyWhenHovered();
+  requestSizeChange();
+  gridButtonClicked();
+}
+
+playGame();
